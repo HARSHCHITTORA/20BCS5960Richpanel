3a664191bf72cc7d77f76007ccb04eed405ada84
chittox <chittoxtech@gmail.com>
2023-08-10 17:08:25 +0530

    demo plane page completed next page payment page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/pages/login/login.component';
 import { RegistrationComponent } from './views/pages/registration/registration.component';
+import { DashboardComponent } from './views/pages/dashboard/dashboard.component';
 
 const routes: Routes = [
  
@@ -12,6 +13,11 @@ const routes: Routes = [
   {
     path: 'signup',
     component: RegistrationComponent,
+  },
+  {
+    path:'dashboard',
+    component: DashboardComponent,
+
   },
 ];
 
--- src/app/views/pages/dashboard/dashboard.component.ts
+++ src/app/views/pages/dashboard/dashboard.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  verified: boolean=false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectedOption: number = 0; // Default selected option is Monthly
+  selectedplan:number=0;
+  selectedCard: number | null = null;
+  selectOption(option: number) {
+    this.selectedOption = option;
+    if(this.selectedOption==0)
+    {
+      this.verified = false;
+    }
+    else if(this.selectedOption==1){
+      this.verified = true;
+    }
+  }
+
+  selectplan(option: number) {
+    this.selectedplan = option;
+  }
+
+
+  selectCard(cardNumber: number): void {
+    if (this.selectedCard === cardNumber) {
+      this.selectedCard = null; // Deselect if already selected
+    } else {
+      this.selectedCard = cardNumber; // Select the clicked card
+    }
   }
 
 }
